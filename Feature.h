71d15162a7ab2878e4db892f1d8f6a334361ab98
Kevin Wonus <sales@wonus.com>
2021-04-26 23:57:01 -0400

    Tweak feature.h

--- Feature.h
+++ Feature.h
@@ -15,12 +15,20 @@ public:
 	CFeature(slicable& feature, UINT32 begin, UINT32 end, char type)
 		: slicable(feature, begin, end), featureType{ type }
 	{
-		this->featureMatchVector = NULL;
+		this->featureVector = NULL;
 	}
 	virtual ~CFeature()
 	{
 		;
 	}
 	const char	featureType;
-	UINT16* featureMatchVector;
+	inline const UINT32* getFeatureVector()
+	{
+		if (featureVector == NULL) {
+			;
+		}
+		return featureVector;
+	}
+private:
+	UINT32* featureVector;
 };
